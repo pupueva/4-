f88a23e43c58ef5b24ea84dd865cd733b79173c0
pupueva <lubapulaeva7@gmail.com>
2024-10-09 19:37:48 +0300

    Add files via upload

--- 4-/лаба 4/4лаба.c
+++ 4-/лаба 4/4лаба.c
@@ -1,7 +1,9 @@
+//4 лаба
+#define _CRT_SECURE_NO_DEPRECATE 
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <locale.h>
-#define _CRT_SECURE_NO_DEPRECATE 
 int main()
 {
     setlocale(LC_CTYPE, "RUS");
@@ -17,16 +19,16 @@ int main()
     printf("вывести %c:\n",c);
     printf("вывести %d:\n", i);
     printf("вывести %f:\n", f);
-    printf("вывести %lf:\n", d);
+    printf("вывести %e:\n", d);
 
-    char cz;
+    char pz;
     int iz;
     float fz;
     double dz;
 
-    printf("ввести cz:");
-    scanf_s("%c",&cz);
-    printf("%c\n", cz);
+    printf("ввести pz:");
+    scanf_s("%c",&pz);
+    printf("%c\n", pz);
 
     printf("ввести iz:");
     scanf_s("%d", &iz);
@@ -37,8 +39,8 @@ int main()
     printf("%f\n", fz);
 
     printf("ввести dz:");
-    scanf_s("%lf", &dz);
-    printf("%lf\n", dz);
+    scanf_s("%le", &dz);
+    printf("%le\n", dz);
     
     //1a
     int pupu2;
@@ -53,22 +55,22 @@ int main()
     double pupu3;
     pupu3 = 1.0 / pupu2;
 
-    printf("\n  дес€тичное число, соотвествующее 1/i  1/%d=%lf\n",pupu2,pupu3);
+    printf("\n  дес€тичное число, соотвествующее 1/i  1/%d = %.2lf\n",pz,pupu3);
 
    // задание 2
     int a = 11;
     int b = 3;
    
-    printf("результат делени€ a/b с преобразовани€ в float:%2f\n результат делени€ a/b с преобразованием в double:%lf\n", (float)a / b, (double)a / b);
+    printf("\nцелочисленное деление:%d\nрезультат делени€ a/b с преобразовани€ в float:%f\n результат делени€ a/b с преобразованием в double:%lf\n",(int)a/b, (float)a / b, (double)a / b);
 
     //задание 3
 
     int pickme;
 
     printf("введите целое 3х значное число :");
-    scanf_S("%d", &pickme);
+    scanf_s("%d", &pickme);
 
-    printf("\nпоследн€€ цифра числа: %d\nперва€ цифра числа: %d\nсумма цифр данного числа: %d\nчисло наоборот:%d%d%d\n", pickme % 10, pickme / 100, (pickme / 100) + (pickme / 10) % 10 + (pickme % 10), pickme % 10, (pickme / 10) % 10, pickme / 100);
+    printf("\nпоследн€€ цифра числа: %d\nперва€ цифра числа: %d\nсумма цифр данного числа: %d\nчисло наоборот:%d%d%d\n", pickme % 10, pickme / 100, (pickme % 10) + (pickme / 10) % 10 +(pickme/100), pickme%10, (pickme/10)%10, pickme/100);
 
     return 0;
 }
